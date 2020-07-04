095d8d130d20c77be46945c700dbad15c12afae2
VidishaHegde <f20180135@goa.bits-pilani.ac.in>
2020-07-05 00:33:13 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MultiplayerComponent } from './multiplayer/multiplayer.component';
+import { SingleplayerComponent } from './singleplayer/singleplayer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MultiplayerComponent,
+    SingleplayerComponent
   ],
   imports: [
     BrowserModule,
