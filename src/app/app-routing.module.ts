5e90d442ea106ee73ea288f9d4ed2310bbd0bb78
VidishaHegde <f20180135@goa.bits-pilani.ac.in>
2020-07-11 21:56:13 +0530

    navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MultiplayerComponent } from './multiplayer/multiplayer.component';
+import { SingleplayerComponent } from './singleplayer/singleplayer.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'multiplayer', component: MultiplayerComponent },
+  { path: 'singleplayer', component: SingleplayerComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MultiplayerComponent } from './multiplayer/multiplayer.component';
 import { SingleplayerComponent } from './singleplayer/singleplayer.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MultiplayerComponent,
-    SingleplayerComponent
+    SingleplayerComponent,
+    HomeComponent,
+    HeaderComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    
+
   ],
-  providers: [],
+  providers: [AppRoutingModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
